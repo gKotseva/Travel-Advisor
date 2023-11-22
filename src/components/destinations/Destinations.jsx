8b4e2553f9df5fcafa9b2ebc098bb88379559306
Gabriela Kotseva <gkotseva98@gmail.com>
2023-11-22 14:16:10 +0200

    Fixed header

--- src/components/destinations/Destinations.jsx
+++ src/components/destinations/Destinations.jsx
@@ -17,21 +17,21 @@ export default function Destinations() {
         </div>
         <div className="article-card">
           <div className="content">
-            <p className="title">Iceland</p>
+            <p className="title">Bulgaria</p>
             <Link to="/">View places<i className="arrow right"></i></Link>
           </div>
           <img
-            src="https://www.nationsonline.org/gallery/World/island-countries.jpg"
+            src="https://media.istockphoto.com/id/1182393363/photo/alexander-nevsky-cathedral-in-sofia-bulgaria-taken-in-may-2019-taken-in-hdr.jpg?s=612x612&w=0&k=20&c=pxdKXGCg5wnU1cpK-0em0FAISx0aV5o18l2FaU5u94M="
             alt="article-cover"
           />
         </div>
         <div className="article-card">
           <div className="content">
-            <p className="title">Iceland</p>
+            <p className="title">Spain</p>
             <Link to="/">View places<i className="arrow right"></i></Link>
           </div>
           <img
-            src="https://www.nationsonline.org/gallery/World/island-countries.jpg"
+            src="https://media.istockphoto.com/id/464975554/photo/toledo-spain.jpg?s=612x612&w=0&k=20&c=MlWOs6-Gn0W8BbKBewRAEE_T01-KCDMa5JhH5sfBIiw="
             alt="article-cover"
           />
         </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,6 +10,7 @@ export default function Header() {
             <Link to="/">Home</Link>
             <Link to="/destinations">Destinations</Link>
             <Link to="/my-destinations">My destinations</Link>
+            <Link to="/my-destinations">Add destination</Link>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/logout">Logout</Link>
