29928de92144c6e5fc7dc23196b80740bac97ecb
Gabriela Kotseva <gkotseva98@gmail.com>
2023-11-24 18:15:36 +0200

    Dynamic rendering added to the destinations page

--- client/src/components/destinations/Destinations.jsx
+++ client/src/components/destinations/Destinations.jsx
@@ -1,40 +1,47 @@
 import "./destinations.modules.css";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Destinations() {
+
+  const [destinations, setDestinations] = useState([]);
+
+  useEffect(() => {
+    const fetchDestinations = async () => {
+      try {
+        const response = await fetch('/api/destinations');
+
+        if(!response.ok){
+          throw new Error(`Internal server error!`)
+        }
+
+        const data = await response.json()
+        console.log(data)
+        setDestinations(data);
+
+      } catch (error) {
+        console.error('Error fetching destinations:', error);
+      }
+    };
+
+    fetchDestinations();
+  }, []);
+
   return (
     <>
       <div className="center">
-        <div className="article-card">
-          <div className="content">
-            <p className="title">Iceland</p>
-            <Link to="/">View places<i className="arrow right"></i></Link>
-          </div>
-          <img
-            src="https://www.nationsonline.org/gallery/World/island-countries.jpg"
-            alt="article-cover"
-          />
-        </div>
-        <div className="article-card">
-          <div className="content">
-            <p className="title">Bulgaria</p>
-            <Link to="/">View places<i className="arrow right"></i></Link>
-          </div>
-          <img
-            src="https://media.istockphoto.com/id/1182393363/photo/alexander-nevsky-cathedral-in-sofia-bulgaria-taken-in-may-2019-taken-in-hdr.jpg?s=612x612&w=0&k=20&c=pxdKXGCg5wnU1cpK-0em0FAISx0aV5o18l2FaU5u94M="
-            alt="article-cover"
-          />
-        </div>
-        <div className="article-card">
-          <div className="content">
-            <p className="title">Spain</p>
-            <Link to="/">View places<i className="arrow right"></i></Link>
-          </div>
-          <img
-            src="https://media.istockphoto.com/id/464975554/photo/toledo-spain.jpg?s=612x612&w=0&k=20&c=MlWOs6-Gn0W8BbKBewRAEE_T01-KCDMa5JhH5sfBIiw="
-            alt="article-cover"
-          />
-        </div>
+        {destinations.map((destination) => 
+                <div className="article-card">
+                <div className="content">
+                  <p className="title">{destination.country}</p>
+                  <Link to="/">View places<i className="arrow right"></i></Link>
+                </div>
+                <img
+                  src={destination.imageUrl}
+                  alt="article-cover"
+                />
+              </div>
+        )}
       </div>
     </>
   );
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-const {PORT, DB_URL} = require('./constants')
-const mongoose = require('mongoose')
 const express = require('express')
+const { default: mongoose } = require('mongoose')
+const Destination = require('./models/destinations')
 
 const app = express()
+const PORT = 8888
+const DB_URL = 'mongodb://localhost:27017/Travel-Advisor'
 
 async function dbConnect(){
     await mongoose.connect(DB_URL)
@@ -13,5 +15,18 @@ dbConnect()
     console.log(`Connected to the database!`)
 })
 .catch((e) => console.log(`Error connecting to the database! Error message: ${e}`))
+
+app.get('/api/destinations', async (req, res) => {
+    try {
+      const destinations = await Destination.find();
+      res.json(destinations);
+    } catch (error) {
+      console.error('Error fetching items:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+
+
 
 app.listen(PORT, console.log(`Server is listening on port: ${PORT}`))
