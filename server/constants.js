e87654e121e201f2355fa54201983352dacb5aed
Gabriela Kotseva <gkotseva98@gmail.com>
2023-11-24 18:05:08 +0200

    Connect to the database

---
+++ server/constants.js
@@ -0,0 +1,6 @@
+constants = {
+    PORT: 8888,
+    DB_URL: 'mongodb://localhost:27017/Travel-Advisor'
+}
+
+module.exports = constants
--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
+const {PORT, DB_URL} = require('./constants')
+const mongoose = require('mongoose')
+
 const express = require('express')
 
 const app = express()
-const PORT = 8888
+
+async function dbConnect(){
+    await mongoose.connect(DB_URL)
+}
+
+dbConnect()
+.then(() => {
+    console.log(`Connected to the database!`)
+})
+.catch((e) => console.log(`Error connecting to the database! Error message: ${e}`))
 
 app.listen(PORT, console.log(`Server is listening on port: ${PORT}`))
