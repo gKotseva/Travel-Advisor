7e0ef18526748bb68426258bba7eb9e050e9e064
Gabriela Kotseva <gkotseva98@gmail.com>
2023-11-28 15:19:18 +0200

    Added constants for the forms

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -3,12 +3,17 @@ import useForm from '../../hooks/useForm';
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/authContext';
 
+const LoginFormKeys = {
+  Email: 'email',
+  Password: 'password'
+}
+
 export default function Login() {
 
   const {loginSubmitHandler} = useContext(AuthContext)
   const {values, onChange, onSubmit} = useForm(loginSubmitHandler, {
-    email: '',
-    password: ''
+    [LoginFormKeys.Email]: '',
+    [LoginFormKeys.Password]: ''
   })
 
   return (
@@ -17,11 +22,11 @@ export default function Login() {
         <h2>Login</h2>
         <form onSubmit={onSubmit}>
           <div className="user-box">
-            <input type="email" name="email" onChange={onChange} value={values.email} />
+            <input type="email" name="email" onChange={onChange} value={values[LoginFormKeys.Email]} />
             <label>Email</label>
           </div>
           <div className="user-box">
-            <input type="password" name="password" onChange={onChange} value={values.password} />
+            <input type="password" name="password" onChange={onChange} value={values[LoginFormKeys.Password]} />
             <label>Password</label>
           </div>
           <input type="submit" value="Login" className='btn'></input>
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -3,14 +3,20 @@ import useForm from '../../hooks/useForm';
 import './register.modules.css'
 import { AuthContext } from '../contexts/authContext';
 
+const RegisterFormKeys = {
+  Email: 'email',
+  Password: 'password',
+  RepeatPassword: 'repeatPassword'
+}
+
 export default function Register() {
 
   const {registerSubmitHandler} = useContext(AuthContext)
 
   const {values, onChange, onSubmit} = useForm(registerSubmitHandler, {
-    email: '',
-    password: '',
-    repeatPassword: '',
+    [RegisterFormKeys.Email]: '',
+    [RegisterFormKeys.Password]: '',
+    [RegisterFormKeys.RepeatPassword]: '',
   })
   return (
     <>
@@ -18,15 +24,15 @@ export default function Register() {
         <h2>Register</h2>
         <form onSubmit={onSubmit}>
           <div className="user-box">
-            <input type="text" name="email" onChange={onChange} value={values.email}/>
+            <input type="text" name="email" onChange={onChange} value={values[RegisterFormKeys.Email]}/>
             <label>Email</label>
           </div>
           <div className="user-box">
-            <input type="password" name="password" onChange={onChange} value={values.password}/>
+            <input type="password" name="password" onChange={onChange} value={values[RegisterFormKeys.Password]}/>
             <label>Password</label>
           </div>
           <div className="user-box">
-            <input type="password" name="repeatPassword" onChange={onChange} value={values.repeatPassword}/>
+            <input type="password" name="repeatPassword" onChange={onChange} value={values[RegisterFormKeys.RepeatPassword]}/>
             <label>Repeat Password</label>
           </div>
           <input type="submit" value="Register" className='btn'></input>
