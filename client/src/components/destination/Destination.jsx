49fb3881052912b9ee7dc2f5a8c7a699e4ae21ae
Gabriela Kotseva <gkotseva98@gmail.com>
2023-12-03 16:30:05 +0200

    Modal updates

--- client/src/components/destination/Destination.jsx
+++ client/src/components/destination/Destination.jsx
@@ -5,7 +5,9 @@ import DestinationModal from "../destinationModal/DestinationModal";
 
 export default function Destination() {
   const [destination, setDestination] = useState({});
-  const [isModalOpen, setModalOpen] = useState(false)
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [destinationName, setDestinationName] = useState("");
+  const [places, setPlaces] = useState({});
   const location = useLocation();
   const pathName = location.pathname.substring(1);
 
@@ -26,33 +28,37 @@ export default function Destination() {
     ? destination.places
     : [];
 
-  
-    const openModal = () => {
-      setModalOpen(true)
-    }
+  const openModal = (destinationTitle) => {
+    setDestinationName(destinationTitle)
+    setPlaces(destination.places);
+    setModalOpen(true);
+  };
 
-    const closeModal = () => {
-      setModalOpen(false)
-    }
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <>
       <div className="center">
         {currentDestinationPlaces.length > 0 ? (
-          currentDestinationPlaces.map((currentDestination) => (
-            <div className="article-card" key={currentDestination.name}>
-              <div className="content">
-                <p className="title">{currentDestination.name}</p>
-                <Link onClick={openModal}>View description<i className="arrow right"></i></Link>
+          currentDestinationPlaces.map((currentDestination) => {
+            const destinationTitle = currentDestination.name;
+            return (
+              <div className="article-card" key={destinationTitle}>
+                <div className="content">
+                  <p className="title">{destinationTitle}</p>
+                  <Link onClick={() => openModal(destinationTitle)}>View description<i className="arrow right"></i></Link>
+                </div>
+                <img src={currentDestination.image} alt="article-cover" />
               </div>
-              <img src={currentDestination.image} alt="article-cover" />
-            </div>
-          ))
+            );
+          })
         ) : (
           <p>No places found for this country.</p>
         )}
       </div>
-      <DestinationModal isOpen={isModalOpen} onClose={closeModal} />
+      <DestinationModal isOpen={isModalOpen} onClose={closeModal} places={places} destinationName={destinationName} />
     </>
   );
 }
--- client/src/components/destinationModal/DestinationModal.jsx
+++ client/src/components/destinationModal/DestinationModal.jsx
@@ -1,14 +1,30 @@
-import './destinationModal.modules.css'
+import { useState, useEffect } from 'react';
+import './destinationModal.modules.css';
 
-export default function DestinationModal({isOpen, onClose}) {
-    if (!isOpen) return null
+export default function DestinationModal({ isOpen, onClose, destinationName, places }) {
+  const [currentPlace, setCurrentPlace] = useState(null);
 
-    return (
+  useEffect(() => {
+    if (Array.isArray(places) && places.find) {
+      const foundPlace = places.find((place) => place.name === destinationName);
+      setCurrentPlace(foundPlace);
+    } else {
+      setCurrentPlace(null);
+    }
+  }, [destinationName, places]);
+
+  if (!isOpen || !currentPlace) return null;
+
+  return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <span className="close-button" onClick={onClose}>[Close]</span>
-        <p>whatever</p>
+        <span className="close-button" onClick={onClose}>
+          [Close]
+        </span>
+        <p className='paragraphName'>{currentPlace.name}</p>
+        <img src={currentPlace.image} alt="article-cover" className='imagePlace'/>
+        <p className='paragraphDescription'>Description: {currentPlace.description}</p>
       </div>
     </div>
-    )
+  );
 }
