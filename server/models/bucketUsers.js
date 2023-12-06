c17deb3b14e3f3432ed61f306fa02b8586376b8f
Gabriela Kotseva <gkotseva98@gmail.com>
2023-12-06 15:09:01 +0200

    bucketList model created

---
+++ server/models/bucketUsers.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const bucketUserSchema = new mongoose.Schema({
+    user: {type: String, required: true, unique: true},
+    place: {type: Array, required: true},
+})
+
+const bucketUser = mongoose.model('bucketUser', bucketUserSchema)
+
+module.exports = bucketUser
