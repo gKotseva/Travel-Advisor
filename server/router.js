26db94cfadc889e96298463745a4b430dfcdb4dd
Gabriela Kotseva <gkotseva98@gmail.com>
2023-12-06 15:10:36 +0200

    Bucket controller added to router

--- server/router.js
+++ server/router.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 
 const userController = require('./controllers/userController')
 const destinationController = require('./controllers/destinationController')
+const bucketController = require('./controllers/bucketController')
 
 router.use('/api/users', userController)
 router.use('/api/destinations', destinationController)
+router.use('/api/bucket', bucketController)
 
 module.exports = router
 
